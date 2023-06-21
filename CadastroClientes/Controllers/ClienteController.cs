c7dbc0f78759a21774f40b2616f99924adc98569
projetoscintia <cpinho@tecnologia-unica.com.br>
2023-06-21 14:09:22 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroClientes/Controllers/ClienteController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroClientes.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create() { return View(); }
+        public IActionResult Edit() { return View(); }
+        public IActionResult Delete() { return View(); }
+
+    }
+}
